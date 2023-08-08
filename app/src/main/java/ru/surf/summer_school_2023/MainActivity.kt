f005fc61d41b90bf3f7120cb5a6f92d038fd24a1
milkfist <milkfist@student.21-school.ru>
2023-08-08 22:16:48 +0300

    if list is empty we are on empty fragment, if with elements - on recyclerviewFragment

--- app/src/main/java/ru/surf/summer_school_2023/MainActivity.kt
+++ app/src/main/java/ru/surf/summer_school_2023/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import ru.surf.summer_school_2023.databinding.ActivityMainBinding
+import ru.surf.summer_school_2023.model.CocktailModel
 import viewmodel.ViewmodelRecyclerviewFragment
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +13,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
 
-    private var cocktailListForFragment = ViewmodelRecyclerviewFragment()
+    private val fragmentRV = ViewmodelRecyclerviewFragment()
+    private var findedFragmentRV = findFragmentRV(fragmentRV)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        if (cocktailListForFragment.loadListFromSharedPreferences(this, "listOfCocktailModel") == emptyList())
-//        goToFragment(NoCoctailsFragment())
-        goToFragment(cocktailListForFragment)
+        if (findedFragmentRV?.loadListFromSharedPreferences(this, "listOfCocktailModel")?.isEmpty() == true) {
+            goToFragment(NoCoctailsFragment())
+        } else {
+            goToFragment(fragmentRV)
+        }
     }
 
     private fun goToFragment(fragment: Fragment) {
         fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().replace(R.id.fragmentNoCoctails, fragment).commit()
+    }
+
+    private fun findFragmentRV(fragment: Fragment): ViewmodelRecyclerviewFragment? {
+        fragmentManager = supportFragmentManager
+        return fragmentManager.findFragmentById(fragment.id) as ViewmodelRecyclerviewFragment?
     }
 }
